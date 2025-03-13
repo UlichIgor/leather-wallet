8f438fe5a2a8c8603662cba6d8bf462f6e8cff1f
Ulich Igor <ulichigor55@gmail.com>
2025-03-13 07:22:14 +0200

    // Content:

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,10 +5,16 @@ export default class Header extends Component {
   render() {
     return (
       <div className={`${styles.header} ${styles.poster}`}>
-        <div className={styles.logo}>
-          <a href="https://github.com/UlichIgor" target="_blank" rel="noopener noreferrer">
-            <img src={Logo} alt="Product Logo" />
-          </a>
+        <div className={styles.container}>
+          <div className={styles.logo}>
+            <a href="https://github.com/UlichIgor" target="_blank" rel="noopener noreferrer">
+              <img src={Logo} alt="Product Logo" />
+            </a>
+          </div>
+          <div className={styles.contacts}>
+            {/* Добавьте здесь контактную информацию */}
+            <button className={styles.phone}><a href='tel:380123456789' className={styles.links}>+380123456789</a></button>
+          </div>
         </div>
         <h1 className={styles.title}>Шкіряні гаманці</h1>
         <p className={styles.slogan}>Найкраща якість для вас</p>
